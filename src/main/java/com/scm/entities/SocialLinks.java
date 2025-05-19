00608def55e0a369dda2d388ad54096694f8d01a
Mehmood-Ul-Haq <mehmoodulhaq570@gmail.com>
2025-05-19 14:52:49 +0500

    Completed Signup page for user

--- src/main/java/com/scm/entities/SocialLinks.java
+++ src/main/java/com/scm/entities/SocialLinks.java
@@ -1,6 +1,8 @@
 package com.scm.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
