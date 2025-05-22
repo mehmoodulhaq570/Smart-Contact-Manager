c12397f8ba954623ac63ac848079cae0b93414b6
Mehmood-Ul-Haq <mehmoodulhaq570@gmail.com>
2025-05-22 23:58:12 +0500

    Sync user with database (inprogress)

--- src/main/java/com/scm/config/SecurityConfig.java
+++ src/main/java/com/scm/config/SecurityConfig.java
@@ -1,34 +1,54 @@
 package com.scm.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class SecurityConfig {
-    private InMemoryUserDetailsManager inMemoryUserDetailsManager;
 
-    // user create and login using java code with memory service
-    public UserDetailsService userDetailsService() {
-        // Create an in-memory user details manager
-        UserDetails user1 = User
-        .withDefaultPasswordEncoder()
-        .username("admin123")
-        .password("admin123")
-        .roles("ADMIN","USER")
-        .build();
+    //private InMemoryUserDetailsManager inMemoryUserDetailsManager;
 
-        UserDetails user2 = User
-        .withDefaultPasswordEncoder()
-        .username("user123")
-        .password("user123")
-        //.roles("ADMIN","USER")
-        .build();
+    // // user create and login using java code with memory service
+    // @Bean
+    // public UserDetailsService userDetailsService() {
+    //     // Create an in-memory user details manager
+    //     UserDetails user1 = User
+    //     .withDefaultPasswordEncoder()
+    //     .username("admin123")
+    //     .password("admin123")
+    //     .roles("ADMIN","USER")
+    //     .build();
 
-        var inMemoryUserDetailsManager = new InMemoryUserDetailsManager(user1, user2);  
-        return inMemoryUserDetailsManager;
+    //     UserDetails user2 = User
+    //     .withDefaultPasswordEncoder()
+    //     .username("user123")
+    //     .password("user123")
+    //     //.roles("ADMIN","USER")
+    //     .build();
+
+    //     var inMemoryUserDetailsManager = new InMemoryUserDetailsManager(user1, user2);  
+    //     return inMemoryUserDetailsManager;
+    // }
+
+    // It has all methods available by which we can register to our service
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        // Get the object of user details service
+        daoAuthenticationProvider.setUserDetailsService(null);
+        // Set the object of password encoder
+        daoAuthenticationProvider.setPasswordEncoder(null);
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
--- src/main/java/com/scm/entities/User.java
+++ src/main/java/com/scm/entities/User.java
@@ -1,9 +1,15 @@
 package com.scm.entities;
 
 import java.util.*;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -11,6 +17,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,12 +31,14 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class User {
+public class User implements UserDetails {
     
     @Id
     private String userId;
     @Column(name = "user_name", nullable = false)
     private String name;
+
+    @Getter(value = AccessLevel.NONE)
     private String password;
     @Column(unique = true, nullable = false)
     private String email;
@@ -41,9 +50,12 @@ public class User {
     private String profilePicture;
     //Information
     @Builder.Default
+    @Getter(value = AccessLevel.NONE)
     private boolean enabled=false;
+
     @Builder.Default
     private boolean emailVerified=false;
+
     @Builder.Default
     private boolean phoneVerified=false;
     // SELF, GOOGLE, FACEBOOK, TWITTER
@@ -55,6 +67,51 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
     @Builder.Default
     private List<Contact> contacts = new ArrayList<>();
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> rolesList = new ArrayList<>();
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        // List of roles [ADMIN, USER]
+        // Collections of simple granted authority [ADMIN, USER]
+        Collection<SimpleGrantedAuthority> roles = rolesList.stream().map(role -> new SimpleGrantedAuthority(role)).collect(Collectors.toList());
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
 
 
 
