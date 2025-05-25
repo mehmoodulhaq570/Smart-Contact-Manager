520ae540b956af3a4519752f3ae576d44a5ef295
Mehmood-Ul-Haq <mehmoodulhaq570@gmail.com>
2025-05-25 23:25:39 +0500

    Login with Github in progress

--- src/main/java/com/scm/config/OAuthAuthenticationSuccessHandler.java
+++ src/main/java/com/scm/config/OAuthAuthenticationSuccessHandler.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -34,38 +35,57 @@ public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessH
             Authentication authentication) throws IOException, ServletException {
                 logger.info("AuthenticationSuccessHandler");
 
-                // Before redirecting fetch the user details and save it in database
-                DefaultOAuth2User oauth2User = (DefaultOAuth2User) authentication.getPrincipal();
-                
-                String email = oauth2User.getAttribute("email").toString();
-                String name = oauth2User.getAttribute("name").toString();
-                String picture = oauth2User.getAttribute("picture").toString();
 
-                logger.info("User Details: Email: {}, Name: {}, Picture: {}", email, name, picture);
+                    logger.info("OAuthAuthenicationSuccessHandler");
 
-                // Create user and save in database
+                // identify the provider
 
-                User user = new User();
-                user.setEmail(email);
-                user.setName(name);
-                user.setProfilePicture(picture);
-                user.setPassword("oauth2password"); 
-                user.setUserId(UUID.randomUUID().toString());
-                user.setProvider(Providers.GOOGLE);
-                user.setEnabled(true);
-                user.setEmailVerified(true);
-                user.setProviderId(user.getName());
-                user.setRolesList(List.of(AppConstants.ROLE_USER)); // Set default role
-                user.setAbout("This is an account created using Google");
-                user.setPhoneNumber("000-000-0000"); // Default phone number
-                user.setPhoneVerified(false);
+                var oauth2AuthenicationToken = (OAuth2AuthenticationToken) authentication;
+
+                String authorizedClientRegistrationId = oauth2AuthenicationToken.getAuthorizedClientRegistrationId();
+
+                logger.info(authorizedClientRegistrationId);
+
+                var oauthUser = (DefaultOAuth2User) authentication.getPrincipal();
+
+                oauthUser.getAttributes().forEach((key, value) -> {
+                    logger.info(key + " : " + value);
+                });
+
                 
 
-                User user2 = userRepositories.findByEmail(email).orElse(null);
-                if (user2 == null) {
-                    userRepositories.save(user);
-                    logger.info("New user created with email: {}" + email);
-                }
+                // // Before redirecting fetch the user details and save it in database
+                // DefaultOAuth2User oauth2User = (DefaultOAuth2User) authentication.getPrincipal();
+                
+                // String email = oauth2User.getAttribute("email").toString();
+                // String name = oauth2User.getAttribute("name").toString();
+                // String picture = oauth2User.getAttribute("picture").toString();
+
+                // logger.info("User Details: Email: {}, Name: {}, Picture: {}", email, name, picture);
+
+                // // Create user and save in database
+
+                // User user = new User();
+                // user.setEmail(email);
+                // user.setName(name);
+                // user.setProfilePicture(picture);
+                // user.setPassword("oauth2password"); 
+                // user.setUserId(UUID.randomUUID().toString());
+                // user.setProvider(Providers.GOOGLE);
+                // user.setEnabled(true);
+                // user.setEmailVerified(true);
+                // user.setProviderId(user.getName());
+                // user.setRolesList(List.of(AppConstants.ROLE_USER)); // Set default role
+                // user.setAbout("This is an account created using Google");
+                // user.setPhoneNumber("000-000-0000"); // Default phone number
+                // user.setPhoneVerified(false);
+                
+
+                // User user2 = userRepositories.findByEmail(email).orElse(null);
+                // if (user2 == null) {
+                //     userRepositories.save(user);
+                //     logger.info("New user created with email: {}" + email);
+                // }
 
                 new DefaultRedirectStrategy().sendRedirect(request, response, "/user/profile");
     }
