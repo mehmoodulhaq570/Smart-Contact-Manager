dffb7fb51cd668f8d0df9eceb34ce339f16f2197
Mehmood-Ul-Haq <mehmoodulhaq570@gmail.com>
2025-05-30 20:01:33 +0500

    Add User Contacts in Progress

--- src/main/java/com/scm/controllers/ContactController.java
+++ src/main/java/com/scm/controllers/ContactController.java
@@ -1,7 +1,10 @@
 package com.scm.controllers;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import com.scm.forms.ContactForm;
 
 
 @Controller
@@ -11,10 +14,18 @@ public class ContactController {
     // Add contact page handler
     
     @RequestMapping("/add")
-    public String addContactView() {
+    public String addContactView(Model model) {
+        ContactForm contactForm = new ContactForm();
+        model.addAttribute("contactForm", contactForm);
         return "user/add_contact"; // This should return the name of the contact page view
     }
 
-
+    // Handle form submission
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addContact(@ModelAttribute ContactForm contactForm) {
+        // Process the form data
+        System.out.println(contactForm);
+        return "redirect:/user/contacts/add"; // Redirect to the contacts list page
+    }
 
 }
---
+++ src/main/java/com/scm/forms/ContactForm.java
@@ -0,0 +1,26 @@
+package com.scm.forms;
+import org.springframework.web.multipart.MultipartFile;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ContactForm {
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String description;
+    private boolean isFavorite;
+    private String websiteLink;
+    private String linkedInLink;
+    private MultipartFile profileimage;
+
+
+}
